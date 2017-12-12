4f1cff1ef55d09d76f1e992bd087750ebe9ca9a9
k1630480 <jaman.salique@kcl.ac.uk>
2017-12-12 14:01:02 +0000

    Add corrected intergration tests
    I have fixed all integration tests

--- test/integration/comments_actions_test.rb
+++ test/integration/comments_actions_test.rb
@@ -3,6 +3,12 @@
  class CommentActionsTest < ActionDispatch::IntegrationTest
  include Devise::Test::IntegrationHelpers
 
-  
+  test "adding a comment to a post" do
+    post = articles(:post_one)
+    post "/articles/#{post.id}/comments", params: {comment: {commenter: "commenter", body: "body",article_id: post.id}}
+    assert_response :redirect
+    follow_redirect!
+    assert_response :success
+  end
   
 end
--- test/integration/users_actions_test.rb
+++ test/integration/users_actions_test.rb
@@ -3,5 +3,45 @@ require 'test_helper'
 class CommentPostingFlowsTest < ActionDispatch::IntegrationTest
 	include Devise::Test::IntegrationHelpers
 
+ # we have only checked that the pages that devise provides
+ # are accessible via our application.
+ # it is not our responsibility to carry out init tests on 
+ # third party code
+ 
+  test "while logged in showing the forgot passwprd page should take to root/index" do
+ 	user = users(:user_one)
+ 	sign_in user
+ 	get "/users/password/new"
+ 	assert_response :redirect
+ 	assert_equal 'You are already signed in.', flash[:alert]
+ end
+ 
+  test "showing the forgot password page" do
+ 	get "/users/password/new"
+ 	assert_response :success
+ end
+ 
+  test "when logged in showing the edit user details page" do
+ 	get "/users/edit"
+ 	assert_redirected_to new_user_session_path
+ 	assert_equal 'You need to sign in or sign up before continuing.', flash[:alert]
+ end
+ 
+  test "showing page for edit user details" do
+ 	user = users(:user_one)
+ 	sign_in user
+ 	get "/users/edit"
+ 	assert_response :success
+ end
+ 
+  test "showing page for signing up page" do
+ 	get "/users/sign_up"
+ 	assert_response :success
+ end
+
+ test "showing page for signing in" do
+ 	get "/users/sign_in"
+ 	assert_response :success
+ end
 
 end
